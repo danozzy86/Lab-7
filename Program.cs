2163a33e52a4d5cbbc4285bd0aae5ad20ecaf184
danozz86 <danozzy86@gmail.com>
2020-11-12 16:26:23 -0500

    dlo
    Lab finished and working.

--- Program.cs
+++ Program.cs
@@ -25,7 +25,9 @@ class Program
                     Thread.Sleep(2000);
                 }
             } catch (Exception ex){
+                Console.WriteLine("The path is invalid.");
                 Console.WriteLine(ex);
+                Thread.Sleep(2000);
             }
         }
         
@@ -42,8 +44,45 @@ class Program
                 teamScore[3] = Convert.ToString(values[3]);
             }
         }
-
         
+        string winner = CheckWinner(teamScore);
+        
+        if (winner == teamScore[0]){
+            Console.WriteLine(teamScore[0] + " wins with a score of " + teamScore[2] + ".");
+            Console.ReadKey();
+        }else if (winner == teamScore[1]){
+            Console.WriteLine(teamScore[1] + " wins with a score of " + teamScore[3] + ".");
+            Console.ReadKey();
+        }else if (winner == "DRAW"){
+            Console.WriteLine("There is a draw with a score of " + teamScore[2] + ".");
+            Console.ReadKey();
+        }
+    }
+    
+    private static string CheckWinner(string[] teamScore){
+        
+        int team1Score = -1;
+        int team2Score = -1;
+
+        try {
+            team1Score = Convert.ToInt32(teamScore[2]);
+            team2Score = Convert.ToInt32(teamScore[3]);
+
+        } catch (Exception ex) {
+            Console.WriteLine("The score is not a valid integer.");
+            Console.WriteLine(ex);
+            Thread.Sleep(2000);
+        }
+
+        if (team1Score > team2Score){
+            return teamScore[0];
+        }
+        else if(team1Score <team2Score){
+            return teamScore[1];
+        }
+        else{
+            return "DRAW";
+        }
     }
 }
 
