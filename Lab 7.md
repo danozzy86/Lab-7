d410deec8b00eae5a0c13d98573365916cc843a5
danozz86 <danozzy86@gmail.com>
2020-11-12 15:49:17 -0500

    dlo

---
+++ Lab 7.md
@@ -0,0 +1,19 @@
+# Score display
+
+Create a program that reads a CSV file inputed by the user. For each entry in the file, display the winning Team and winning score. CSV file format; (Team 1, Team 2, team 1 score, team 2 score)
+
+### Possiable points:
+Each Requirement is worth 2 points totaling to 10 possiable points. 
+
+### Due Date:
+11/12/2020 @ 4:00PM (Beginning of class)
+
+### How to upload:
+Zip all files into a compressed folder and upload to BB with the folder named "(LastName)Lab1.zip or upload a word document with a GitHub link included. If the link does not work I will email you and you will have 1 day (24 hours) of when I email you to provide a working link or send me a zip file matching the terms above.
+
+### Requirements:
+* Program works correctly with no errors.
+* Program correctly determins the winner between the two teams.
+* Code is clean/readable as described in class
+* Commented code (Comments describing what the code is doing. With out over commenting!)
+* Project is submitted correctly as described above.
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool validPath = false;
+        string path = "";
+        string[] teamScore = new string[6];
+
+        //Collects and validates the csv file path from the user.
+        while(!validPath){
+            Console.Clear();
+
+            Console.WriteLine("What is the path of the desired score.csv file?");
+            path = Convert.ToString(Console.ReadLine());
+
+            try {
+                if (File.Exists(path)){
+                    validPath = true;
+                } else {
+                    Console.WriteLine("The path is invalid.");
+                    Thread.Sleep(2000);
+                }
+            } catch (Exception ex){
+                Console.WriteLine(ex);
+            }
+        }
+        
+        //Now we use the stream reader to read the csv file data into an array.
+        using(var reader = new StreamReader(path)){
+
+            while(!reader.EndOfStream){
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                teamScore[0] = Convert.ToString(values[0]);
+                teamScore[1] = Convert.ToString(values[1]);
+                teamScore[2] = Convert.ToString(values[2]);
+                teamScore[3] = Convert.ToString(values[3]);
+            }
+        }
+
+        
+    }
+}
+
